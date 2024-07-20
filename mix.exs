adf6235fc57715edb15907d0f71153666f5014df
jeryldev <jeryldev@gmail.com>
2024-07-20 17:35:25 +0800

    add dialyzer dependency; add dialyzer to the github action workflow

--- mix.exs
+++ mix.exs
@@ -60,7 +60,8 @@ defmodule Jeryldevcms.MixProject do
       {:dns_cluster, "~> 0.1.1"},
       {:bandit, "~> 1.5"},
       {:credo, "~> 1.7.7", only: [:dev, :test], runtime: false},
-      {:sobelow, "~> 0.13.0", only: [:dev, :test], runtime: false}
+      {:sobelow, "~> 0.13.0", only: [:dev, :test], runtime: false},
+      {:dialyxir, "~> 1.4.3", only: [:dev, :test], runtime: false}
     ]
   end
 
