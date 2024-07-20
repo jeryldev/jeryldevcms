0d73691629e9b48cbc779851cfc819182bc404eb
jeryldev <jeryldev@gmail.com>
2024-07-20 17:16:51 +0800

    add sobelow dependency; add sobelow step to the github workflow; add a default-src self, content-security-policy

--- mix.exs
+++ mix.exs
@@ -59,7 +59,8 @@ defmodule Jeryldevcms.MixProject do
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.1.1"},
       {:bandit, "~> 1.5"},
-      {:credo, "~> 1.7.7", only: [:dev, :test], runtime: false}
+      {:credo, "~> 1.7.7", only: [:dev, :test], runtime: false},
+      {:sobelow, "~> 0.13.0", only: [:dev, :test], runtime: false}
     ]
   end
 
